5353bcdfeaf2b9d1a6bedbc7a7050158fcafb80b
dacatap <dmcas07@outlook.com>
2026-08-28 00:15:18 -0400

    v0.1.0-alpha
    Start of development.
    -ExtractSteam file implementing GET calls to get SteamLibrary game ID list, newsletter entries per game ID, and review history.

---
+++ scripts/ExtractSteam.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import datetime, timezone
+from typing import Any
+
+#TODO
+#Refactor storage keys for better allocation and clarity, should look more like "raw/Steam/..."
+
+#GET game library dictionary
+def getSteamLibrary(steam_api_key: str, steam_id: str) -> list[int]:
+    url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+
+    params = {
+        "key": steam_api_key,
+        "steamid": steam_id,
+        "format": "json",
+        "include_appinfo": 0
+    }
+
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    games = data.get("response", {}).get("games", [])
+    game_id_list = [game["appid"] for game in games]
+
+    return game_id_list
+
+
+#GET review history (last 2 weeks) 
+def getGameSteamReviewHistory(steam_game_id:int) -> tuple[str, dict[str, Any]]:
+    url = f"https://store.steampowered.com/appreviewhistogram/{steam_game_id}"
+    response = requests.get(url)
+    response.raise_for_status()
+
+    # Raw payload ready to land directly into S3
+    raw_payload = response.json()
+    fetch_date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+
+    storage_key = f"raw/game_id={steam_game_id}/{fetch_date}.json"
+
+    return storage_key, raw_payload
+
+#GET Steam Newsletter entries, only ones by the dev/publisher
+def getGameSteamNews(steam_game_id: int, count: int = 20) -> tuple[str, dict[str, Any]]:
+    url = "https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/"
+
+    params = {
+        "appid": steam_game_id,
+        "count": count,
+        "maxlength": 300,
+        "format": "json",
+        "feeds": "steam_community_announcements",
+    }
+
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    #Same as previous function, raw payload to be stored into S3
+    raw_payload = response.json()
+    fetch_date = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+    storage_key = f"raw/game_id={steam_game_id}/{fetch_date}news.json"
+
+    return storage_key, raw_payload
---
+++ scripts/Main.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+steam_api_key = os.getenv("STEAM_API_KEY")
+steam_id = os.getenv("STEAM_ID")
+
